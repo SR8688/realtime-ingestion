8d71102c35f3616bea17351859a4ad7f2a12e61a
Shadman Raihan <55094999+SR8688@users.noreply.github.com>
2026-03-01 12:48:58 -0500

    Add ingestion main with simulators, workers and shutdown

--- cmd/ingest/main.go
+++ cmd/ingest/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"realtime-ingestion/internal/db"
+	"realtime-ingestion/internal/message"
+	"realtime-ingestion/internal/simulator"
+	"realtime-ingestion/internal/worker"
+	"syscall"
+	"time"
+)
 
+func main() {
+	var log *slog.Logger = slog.New(slog.NewTextHandler(os.Stdout, nil))
+	ctx := context.Background()
+	sigCtx, cancel := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	var queue chan message.Data = make(chan message.Data, 10)
+	var db db.DB = db.NewMessageStore()
+
+	simulatorManger := simulator.NewSimulatorManger(sigCtx, queue)
+	simulatorManger.SpawnMultiple(3, time.Second*10)
+
+	workerManger := worker.NewWorkerManager(sigCtx, queue, db)
+	workerManger.SpawnMultiple(5)
+
+	<-sigCtx.Done()
+	simulatorManger.StopAll()
+	close(queue)
+	workerManger.StopAll()
+
+	log.Info("exiting the program")
 }
